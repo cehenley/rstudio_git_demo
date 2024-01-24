5fc46e3f1512161b0664badc0dcef5329b74329d
Charla Orozco <cehenley@unm.edu>
2024-01-24 14:56:45 -0700

    summary stats and plot mpg to hp

--- scripts/cars_demo.R
+++ scripts/cars_demo.R
@@ -6,3 +6,15 @@ data('mtcars')
 
 #view dataset
 View(mtcars)
+
+# 4step process/workflow
+#1. pull at least once when you start
+#2. add files to stage them for next snapshot
+#3. commit snapshots - this adds snapshot to history
+#4. push commits to the remote (origin)
+
+#summary stats
+summary(mtcars)
+
+#plot mpg to hp
+plot(mtcars$mpg,mtcars$hp)
